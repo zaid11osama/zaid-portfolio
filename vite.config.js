56da929bcc4b64356703e03346b0c2cc6054719b
zaid11osama <zaidosama235@gmail.com>
2025-11-21 16:49:31 +0300

    Add base for GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -2,12 +2,19 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
+
 export default defineConfig({
   plugins: [react()],
+
+  
+  base: "/zaid-portfolio/",   
+
   server: {
     port: 3000,
     open: true
-  },
+  }
+})
+
   build: {
     outDir: 'dist',
     assetsDir: 'assets',
